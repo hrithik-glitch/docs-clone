669657dff10fa6d641cfb13d945ff92aa2e4fb2a
hrithik-glitch <70329322+hrithik-glitch@users.noreply.github.com>
2021-08-06 20:28:12 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ function Header() {
 				<Icon name="menu" size="3xl" />
 			</Button>
 			<Icon name="description" size="5xl" color="blue" />
-			<h1 className="inline-flex ml-2 text-gray-700 text-2xl cursor-pointer">
+			<h1 className="hidden md:inline-flex ml-2 text-gray-700 text-2xl cursor-pointer">
 				Docs
 			</h1>
 			<div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
